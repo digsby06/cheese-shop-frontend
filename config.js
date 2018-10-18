595e6e0811b738c69a84eaac7ab9471def1fb8e5
Terrence <terr.whittakerjr@gmail.com>
2018-10-18 13:54:36 -0400

    Revert "Initial commit"
    This reverts commit 1ff247ba06e8aa1665bd9f744243743502e8f47d.

--- config.js
+++
@@ -1,5 +0,0 @@
-import api_server from "./api";
-
-const api = api_server;
-
-export default api;
--- next.config.js
+++
@@ -1,8 +0,0 @@
-const withSass = require('@zeit/next-sass')
-
-//const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = withSass({
-    cssModules: false
-//    assetPrefix: isProd ? '/tm-nextjs-starter' : ''
-})
